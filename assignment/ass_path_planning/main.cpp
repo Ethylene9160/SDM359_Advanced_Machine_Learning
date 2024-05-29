e37adc880c29abc0758964ad300393a398bdf9ca
Spark Knight <jianji2019@foxmail.com>
2024-05-29 08:20:48 +0800

    REFRACTOR: Update code style.

--- assignment/ass_path_planning/main.cpp
+++ assignment/ass_path_planning/main.cpp
@@ -1,11 +1,20 @@
 #include<bits/stdc++.h>
 // #include"planner.h"
 #include"enhanced_evo_planner.h"
-using std::cout;
 using std::cin;
+using std::cout;
+std::unordered_map<int, std::string> id2name = {
+    {0, "A"},
+    {1, "B"},
+    {2, "C"},
+    {3, "D"},
+    {4, "E"},
+    {5, "F"},
+    {6, "G"}
+};
 
 int main(int a, char* args[]){
-    evo_planner p;
+    evo_planner p(30);
     int cost = 0;
     // p.findMinCircle(0, cost);
     // p.dpmethod();
@@ -15,8 +24,9 @@ int main(int a, char* args[]){
 
     cout<<"results showing: \n";
     for(int i = 0; i < output.size(); ++i){
-        cout << output[i] << " ";
+        cout << id2name[output[i]] << " -> ";
     }
+    cout << id2name[output[0]];
     cout << "\ncost: " << cost << std::endl;
 
     return 0;
